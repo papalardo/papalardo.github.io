4d6028c1e904dc74c8b0601e1287f61cd4691769
Pablo Papalardo <papalardo94@hotmail.com>
2020-11-06 20:32:55 -0300

    Ajust layout

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 directory=dist
 branch=gh-pages
+
 build_command() {
   yarn build
 }
 
-echo -e "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory
 
-echo -e "\033[0;32mChecking out $branch....\033[0m"
-git worktree add $directory $branch
+git checkout --orphan gh-pages
 
-echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
 
-echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-cd $directory &&
-  git add --all &&
-  git commit -m "Deploy updates" &&
-  git push origin $branch
+git --work-tree dist add --all
 
-echo -e "\033[0;32mCleaning up...\033[0m"
-git worktree remove $directory
+git --work-tree dist commit -m 'Deploy'
+
+git push origin HEAD:gh-pages --force
+
+rm -r $directory
+
+git checkout -f master
+
+git branch -D gh-pages
