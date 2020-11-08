6452eef4036edd6c0cba23eca3cfe0e280633b06
Pablo Papalardo <papalardo94@hotmail.com>
2020-11-08 20:15:59 -0300

    Menu fix bug

--- src/js/linkSmooth.js
+++ src/js/linkSmooth.js
@@ -2,19 +2,17 @@ import { DOMReady } from './helpers'
 import { closeMenu } from './menu'
 import { scroll, getElementOffsetTop } from './helpers/scrollTo'
 
-let elements = {}
-
 const applySmooth = () => {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-
-        elements[anchor.getAttribute('href')] = getElementOffsetTop(document.querySelector(anchor.getAttribute('href')))
 
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
             
             closeMenu()
             
-            setTimeout(() => scroll(elements[this.getAttribute('href')], 500), 500)
+            setTimeout(() => scroll(
+                getElementOffsetTop(document.querySelector(anchor.getAttribute('href')))
+            , 500), 500)
         });
     });
 }
